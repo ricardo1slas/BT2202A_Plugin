d9b367c23fa05f5cc92aacd30749a782c4b41eb7
Farideh2 <rigeldjg@gmail.com>
2024-10-23 12:52:04 -0600

    added more stuff

---
+++ YushiProject/BT2202.cs
@@ -0,0 +1,78 @@
+//Copyright 2012-2019 Keysight Technologies
+//
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//
+//http://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+using System;
+using OpenTap;
+
+// Note this template assumes that you have a SCPI based instrument, and accordingly
+// extends the ScpiInstrument base class.
+
+namespace OpenTap.Plugins.PluginDevelopment
+{
+     [Display("BT2202A", Group: "OpenTap.Plugins.Carga", Description: "Equipo para carga y descarga")]
+    public class BT2202A : ScpiInstrument
+    public class ScpiInstrumentExample : ScpiInstrument
+    {
+        // ScpiInstuments provides useful settings for VisaAddress, IdnString and others.
+        // Anything not already included in ScpiInstrument can be added here.
+
+        public ScpiInstrumentExample()
+        {
+            // Set the name of the Scpi Instrument Example
+            Name = "BT2202A";
+            // Set default values for properties / settings.
+            VisaAddress = "TCPIP0::10.10.10.10::inst0::INSTR" ;
+        }
+
+        /// <summary>
+        /// Open procedure for the instrument.
+        /// </summary>
+        public override void Open()
+        {
+            base.Open();
+            string Identifier = ScpiQuery("*IDN?");
+            Log.Info(Identifier);
+
+            // Use this to ensure the correct instrument is being connected to.
+            if (!IdnString.Contains("Instrument ID"))
+            {
+                Log.Error("This instrument driver does not support the connected instrument.");
+                throw new ArgumentException("Wrong instrument type.");
+            }
+        }
+
+        // Add wrapper methods as needed.
+        /*public void Configure(double centerFrequency, double frequencySpan, int points)
+        {
+            // Use ScpiCommand to send SCPI strings to the instrument.
+            ScpiCommand("SENSE:FREQ:CENT " + centerFrequency);
+            ScpiCommand("SENSE: FREQ:SPAN " + frequencySpan);
+            ScpiCommand("SENSE:SWE:POIN " + points);
+        }
+
+        public double[] SweepMeasurement()
+        {
+            // Use ScpiQuery to read back from the device.
+            return ScpiQuery<double[]>("READ:SAN1?");
+        }*/
+
+        /// <summary>
+        /// Close procedure for the instrument.
+        /// </summary>
+        public override void Close()
+        {
+            // Add code to close the connection to the instrument here.
+            base.Close();
+        }
+    }
+}
